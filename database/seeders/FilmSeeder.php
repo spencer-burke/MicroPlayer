c34cfe6676654786e297f51efcb7973eb773e3e7
spencer burke <info.spencer.burke@gmail.com>
2025-09-28 16:08:34 -0700

    Add Factory and Seeder for Films

--- database/seeders/FilmSeeder.php
+++ database/seeders/FilmSeeder.php
@@ -4,19 +4,16 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
+use App\Models\Film;
 
-class UserSeeder extends Seeder
+class FilmSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        // make a user with 3 profiles
-        User::factory()
-            ->count(2)
-            ->withProfiles(3)
-            ->create();
+        //
+       Film::factory()->count(10)->create();
     }
 }
