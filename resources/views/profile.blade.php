92465131c6ba75a57cb16203b63e9b7c1e7adebc
spencer burke <info.spencer.burke@gmail.com>
2025-10-08 00:16:54 -0700

    Add the profile dashboard with all of the basic information required

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -14,8 +14,19 @@
         <p>{{$recommendation->film_title}}</p>
     @endforeach
     <h3>Film Favorites:</h3>
+    @foreach ($favorites as $favorite)
+       <p>{{$favorite->film_id}}</p>
+       <p>{{$favorite->film_title}}</p>
+    @endforeach
     <h3>Film Watch Laters</h3>
+    @foreach ($watchLaters as $watchLater)
+        <p>{{$watchLater->film_id}}</p>
+        <p>{{$watchLater->film_title}}</p>
+    @endforeach
     <h3>Search History:</h3>
+    @foreach($searchHistories as $searchHistory)
+        <p>{{$searchHistory->search_query}}</p>
+    @endforeach
 </body>
 
 </html>
