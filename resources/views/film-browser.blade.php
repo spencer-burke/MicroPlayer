46690b4aa6fb0bbee9309b0b7894f02dfb51382e
spencer burke <info.spencer.burke@gmail.com>
2025-11-28 18:45:48 -0800

    Add the new views for film watching and film browsing

---
+++ resources/views/film-browser.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>The Film Browser</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+
+<body>
+    <h1>The Film Browser</h1>
+
+    {{-- film thumbnail --}}
+    {{-- link to film viewer(this will add to watch history upon page visit) --}}
+    {{-- add to favorites button --}}
+    {{-- add to watch later button --}}
+</body>
+
+</html>
