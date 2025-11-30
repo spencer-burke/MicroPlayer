99fa0f7b6dc4106d2b9317396983e1b7b270cdde
spencer burke <info.spencer.burke@gmail.com>
2025-11-29 23:09:51 -0800

    Add the page to show the search results

---
+++ resources/views/search-results.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>The Search Results</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+
+<body>
+    <h1>The results from the search</h1>
+</body>
+
+</html>
