20cc5e1330ebb4e7359eccd2b1d4d87f7f71785a
spencer burke <info.spencer.burke@gmail.com>
2025-11-30 20:39:12 -0800

    Add the proper name for the playback id in the films config file

--- config/films.php
+++ config/films.php
@@ -3,14 +3,14 @@
 return [
     [
         'title' => env('MUX_FILM_1_TITLE'),
-        'mux_playback_id' => env('MUX_FILM_1_PLAYBACK_ID'),
+        'playback_id' => env('MUX_FILM_1_PLAYBACK_ID'),
     ],
     [
         'title' => env('MUX_FILM_2_TITLE'),
-        'mux_playback_id' => env('MUX_FILM_2_PLAYBACK_ID'),
+        'playback_id' => env('MUX_FILM_2_PLAYBACK_ID'),
     ],
     [
         'title' => env('MUX_FILM_3_TITLE'),
-        'mux_playback_id' => env('MUX_FILM_3_PLAYBACK_ID'),
+        'playback_id' => env('MUX_FILM_3_PLAYBACK_ID'),
     ],
 ];
